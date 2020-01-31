11951c668cf44892f7ea001be52bc2bf38942aa0
Edward Williams <eddie_w38@yahoo.com>
2020-01-31 00:08:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,17 +2,23 @@ require .pry
 
 def roll_call_dwarves# code an argument here
   # Your code here
+  #.collect
+  
 end
 
 def summon_captain_planet# code an argument here
   # Your code here
+  #.map
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
+  #.find
 end
 
 def find_the_cheese(item)
+  # Your code here
+  #.include?
   cheese_types = ["cheddar", "gouda", "camembert", "cheesy_thing"]
   item.find do |food_item|
     cheese_types.include?(food_item)
